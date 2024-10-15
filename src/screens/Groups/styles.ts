dde276934ec4af8a47746de1b2354efd5503f20a
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-10-14 21:04:36 -0300

    Adding theme, screen Groups and loading component

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background-color: #000;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+color: #fff;
+font-size: 32px;
+`
