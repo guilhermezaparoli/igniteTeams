fcbf85a376a45da1b8c7e95de968f1fc8c52618c
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-10-14 21:46:58 -0300

    Creating Header component

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -5,7 +5,6 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   flex: 1;
   align-items: center;
-  justify-content: center;
 `;
 
 export const Title = styled.Text`
